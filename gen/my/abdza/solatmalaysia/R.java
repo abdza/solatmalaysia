33e29a64be5bf709145048dc92a2304243755a4a
Abdullah Zainul Abidin <abdullah.zainul@gmail.com>
2011-06-18 14:16:10 +0800

    Finally selection of negeri and kawasan works fully

--- gen/my/abdza/solatmalaysia/R.java
+++ gen/my/abdza/solatmalaysia/R.java
@@ -19,16 +19,18 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int asar_time=0x7f060005;
-        public static final int imsak_time=0x7f060001;
-        public static final int isya_time=0x7f060007;
-        public static final int maghrib_time=0x7f060006;
-        public static final int select_kawasan=0x7f060009;
-        public static final int select_negeri=0x7f060008;
+        public static final int asar_time=0x7f060007;
+        public static final int imsak_time=0x7f060003;
+        public static final int isya_time=0x7f060009;
+        public static final int kawasan=0x7f060002;
+        public static final int maghrib_time=0x7f060008;
+        public static final int negeri=0x7f060001;
+        public static final int select_kawasan=0x7f06000b;
+        public static final int select_negeri=0x7f06000a;
         public static final int select_zone=0x7f060000;
-        public static final int subuh_time=0x7f060002;
-        public static final int syuruk_time=0x7f060003;
-        public static final int zohor_time=0x7f060004;
+        public static final int subuh_time=0x7f060004;
+        public static final int syuruk_time=0x7f060005;
+        public static final int zohor_time=0x7f060006;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- src/my/abdza/solatmalaysia/waktusolat.java
+++ src/my/abdza/solatmalaysia/waktusolat.java
@@ -21,7 +21,7 @@ public class waktusolat extends Activity implements OnClickListener {
 	private static final String WAKTU_PREF = "savedwaktu";
 
 	private TextView imsak_time, subuh_time, syuruk_time, zohor_time,
-			asar_time, maghrib_time, isya_time;
+			asar_time, maghrib_time, isya_time,kawasan,negeri;
 	public static final String waktu_imsak = "waktu_imsak";
 	public static final String waktu_subuh = "waktu_subuh";
 	public static final String waktu_syuruk = "waktu_syuruk";
@@ -29,7 +29,7 @@ public class waktusolat extends Activity implements OnClickListener {
 	public static final String waktu_asar = "waktu_asar";
 	public static final String waktu_maghrib = "waktu_maghrib";
 	public static final String waktu_isya = "waktu_isya";
-	public static final String last_update = "last_update";
+	public static final String last_update = "last_update";	
 
 	private Handler guiThread;
 	private ExecutorService waktuThread;
@@ -40,12 +40,24 @@ public class waktusolat extends Activity implements OnClickListener {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+		setContentView(R.layout.main);		
 		
+		if (getIntent().hasExtra("kod_kawasan")) {
+			Bundle extras = getIntent().getExtras();
+			String kod_kawasan = extras.getString("kod_kawasan");
+			getPreferences(MODE_PRIVATE).edit().putString("kod_kawasan", kod_kawasan).commit();
+			
+			String kawasan = extras.getString("kawasan");
+			getPreferences(MODE_PRIVATE).edit().putString("kawasan", kawasan).commit();
+			
+			String negeri = extras.getString("negeri");
+			getPreferences(MODE_PRIVATE).edit().putString("negeri", negeri).commit();
+			Log.d(TAG, "kod_kawasan:" + kod_kawasan);
+		}
 		View selectZoneButton = findViewById(R.id.select_zone);
 		selectZoneButton.setOnClickListener(this);
-		
-		initThreading();		
+
+		initThreading();
 		findViews();
 		guiThread.post(updatewaktu);
 	}
@@ -58,6 +70,11 @@ public class waktusolat extends Activity implements OnClickListener {
 		asar_time = (TextView) findViewById(R.id.asar_time);
 		maghrib_time = (TextView) findViewById(R.id.maghrib_time);
 		isya_time = (TextView) findViewById(R.id.isya_time);
+		negeri = (TextView) findViewById(R.id.negeri);
+		kawasan = (TextView) findViewById(R.id.kawasan);
+		
+		negeri.setText(getPreferences(MODE_PRIVATE).getString("negeri", "Selangor Dan Wilayah Persekutuan"));
+		kawasan.setText(getPreferences(MODE_PRIVATE).getString("kawasan", "Kuala Lumpur"));
 	}
 
 	private void initThreading() {
@@ -77,36 +94,44 @@ public class waktusolat extends Activity implements OnClickListener {
 			}
 		};
 	}
-	
-	public void savewaktu(String nama_waktu,String waktu) {
-		getPreferences(MODE_PRIVATE).edit().putString(nama_waktu, waktu).commit();
+
+	public void savewaktu(String nama_waktu, String waktu) {
+		getPreferences(MODE_PRIVATE).edit().putString(nama_waktu, waktu)
+				.commit();
 	}
 
 	public void updatewaktu() {
 		guiThread.post(new Runnable() {
 			public void run() {
-				imsak_time.setText(getPreferences(MODE_PRIVATE).getString(waktu_imsak,"--"));
-				subuh_time.setText(getPreferences(MODE_PRIVATE).getString(waktu_subuh,"--"));
-				syuruk_time.setText(getPreferences(MODE_PRIVATE).getString(waktu_syuruk,"--"));
-				zohor_time.setText(getPreferences(MODE_PRIVATE).getString(waktu_zohor,"--"));
-				asar_time.setText(getPreferences(MODE_PRIVATE).getString(waktu_asar,"--"));
-				maghrib_time.setText(getPreferences(MODE_PRIVATE).getString(waktu_maghrib,"--"));
-				isya_time.setText(getPreferences(MODE_PRIVATE).getString(waktu_isya,"--"));
+				imsak_time.setText(getPreferences(MODE_PRIVATE).getString(
+						waktu_imsak, "--"));
+				subuh_time.setText(getPreferences(MODE_PRIVATE).getString(
+						waktu_subuh, "--"));
+				syuruk_time.setText(getPreferences(MODE_PRIVATE).getString(
+						waktu_syuruk, "--"));
+				zohor_time.setText(getPreferences(MODE_PRIVATE).getString(
+						waktu_zohor, "--"));
+				asar_time.setText(getPreferences(MODE_PRIVATE).getString(
+						waktu_asar, "--"));
+				maghrib_time.setText(getPreferences(MODE_PRIVATE).getString(
+						waktu_maghrib, "--"));
+				isya_time.setText(getPreferences(MODE_PRIVATE).getString(
+						waktu_isya, "--"));
 			}
 		});
 	}
-	
+
 	public String getkodkawasan() {
-		return getPreferences(MODE_PRIVATE).getString("kod_kawasan","sgr03");
+		return getPreferences(MODE_PRIVATE).getString("kod_kawasan", "sgr03");
 	}
 
 	@Override
 	public void onClick(View v) {
-		switch(v.getId()) {
+		switch (v.getId()) {
 		case R.id.select_zone:
-			Intent i = new Intent(this, ZonSolat.class);			
+			Intent i = new Intent(this, ZonSolat.class);
 			startActivity(i);
 			break;
-		}		
+		}
 	}
 }
