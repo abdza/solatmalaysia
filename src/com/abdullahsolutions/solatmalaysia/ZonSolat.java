633de015710c51dbaf372959605e2460a01b69ee
Abdullah Zainul Abidin <abdullah.zainul@gmail.com>
2011-06-29 01:56:47 +0800

    Added highlight of the current waktu solat

---
+++ src/com/abdullahsolutions/solatmalaysia/ZonSolat.java
@@ -0,0 +1,158 @@
+package com.abdullahsolutions.solatmalaysia;
+
+import com.abdullahsolutions.solatmalaysia.R;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class ZonSolat extends Activity implements OnClickListener {
+
+	private static final String TAG = "ZonSolat";
+
+	private static final String[] negeri = { "Johor", "Kedah", "Kelantan",
+			"Melaka", "Negeri Sembilan", "Pahang", "Perak","Perlis",
+			"Pulau Pinang", "Sabah", "Sarawak",
+			"Selangor Dan Wilayah Persekutuan", "Terengganu",
+			"Wilayah Persekutuan" };
+
+	private static final String[][] nama_kawasan = {
+			{ "Kota Tinggi, Mersing, Johor Bahru",
+					"Batu Pahat, Muar, Segamat, Gemas", "Kluang dan Pontian",
+					"Pulau Aur dan Pemanggil" },
+			{ "Puncak Gunung Jerai", "Kota Setar, Kubang Pasu, Pokok Sena",
+					"Langkawi", "Pendang, Kuala Muda, Yan",
+					"Padang Terap, Sik, Baling", "Kulim, Bandar Baharu" },
+			{
+					"Jeli, Gua Musang (Mukim Galas, Bertam)",
+					"K.Bharu,Bachok,Pasir Puteh,Tumpat,Pasir Mas,Tnh. Merah,Machang,Kuala Krai,Mukim Chiku" },
+			{ "Bandar Melaka, Alor Gajah, Jasin, Masjid Tanah, Merlimau, Nyalas" },
+			{ "Port Dickson, Seremban, Kuala Pilah, Jelebu, Rembau",
+					"Jempol, Tampin" },
+			{ "Genting Sempah, Janda Baik, Bukit Tinggi",
+					"Bentong, Raub, Kuala Lipis",
+					"Maran, Chenor, Temerloh, Bera, Jerantut",
+					"Bukit Fraser, Genting Higlands, Cameron Higlands",
+					"Kuantan, Pekan, Rompin, Muadzam Shah", "Pulau Tioman" },
+			{
+					"Bukit Larut",
+					"Ipoh, Batu Gajah, Kampar, Sg. Siput dan Kuala Kangsar",
+					"Tapah,Slim River dan Tanjung Malim",
+					"Pengkalan Hulu, Grik dan Lenggong",
+					"Temengor dan Belum",
+					"Teluk Intan, Bagan Datoh, Kg.Gajah,Sri Iskandar, Beruas,Parit,Lumut,Setiawan dan Pulau Pangkor",
+					"Selama, Taiping, Bagan Serai dan Parit Buntar" },
+			{ "Kangar, Padang Besar, Arau" },
+			{ "Seluruh Negeri Pulau Pinang" },
+			{
+					"Zon 9 - Sipitang, Membakut, Beaufort, Kuala Penyu, Weston, Tenom, Long Pa Sia",
+					"Zon 8 - Pensiangan, Keningau, Tambunan, Nabawan",
+					"Zon 7 - Papar, Ranau, Kota Belud, Tuaran, Penampang, Kota Kinabalu",
+					"Zon 6 - Gunung Kinabalu",
+					"Zon 5 - Kudat, Kota Marudu, Pitas, Pulau Banggi",
+					"Zon 3 - Lahad Datu, Kunak, Silabukan, Tungku, Sahabat, Semporna",
+					"Zon 2 - Pinangah, Terusan, Beluran, Kuamut, Telupit",
+					"Zon 1 - Sandakan, Bdr. Bkt. Garam, Semawang, Temanggong, Tambisan",
+					"Zon 4 - Tawau, Balong, Merotai, Kalabakan" },
+			{
+					"Zon 1 - Limbang, Sundar, Terusan, Lawas",
+					"Zon 8 - Kuching, Bau, Lundu,Sematan",
+					"Zon 7 - Samarahan, Simunjan, Serian, Sebuyau, Meludam",
+					"Zon 6 - Kabong, Lingga, Sri Aman, Engkelili, Betong, Spaoh, Pusa, Saratok, Roban, Debak",
+					"Zon 5 - Belawai, Matu, Daro, Sarikei, Julau, Bitangor, Rajang",
+					"Zon 4 - Igan, Kanowit, Sibu, Dalat, Oya",
+					"Zon 3 - Song, Belingan, Sebauh, Bintulu, Tatau, Kapit",
+					"Zon 2 - Niah, Belaga, Sibuti, Miri, Bekenu, Marudi" },
+			{ "Gombak,H.Selangor,Rawang,H.Langat,Sepang,Petaling,S.Alam",
+					"Sabak Bernam, Kuala Selangor, Klang, Kuala Langat",
+					"Kuala Lumpur", "Putrajaya" },
+			{ "Kuala Terengganu, Marang", "Kemaman Dungun", "Hulu Terengganu",
+					"Besut, Setiu" }, { "Labuan" } };
+
+	private static final String[][] kod_kawasan = {
+			{ "jhr02", "jhr04", "jhr03", "jhr01" },
+			{ "kdh06", "kdh01", "kdh05", "kdh02", "kdh03", "kdh04" },
+			{ "ktn03", "ktn01" },
+			{ "mlk01" },
+			{ "ngs02", "ngs01" },
+			{ "phg05", "phg04", "phg03", "phg06", "phg02", "phg01" },
+			{ "prk07", "prk02", "prk01", "prk03", "prk04", "prk05", "prk06" },
+			{ "pls01" },
+			{ "png01" },
+			{ "sbh09", "sbh08", "sbh07", "sbh06", "sbh05", "sbh03", "sbh02",
+					"sbh01", "sbh04" },
+			{ "swk01", "swk08", "swk07", "swk06", "swk05", "swk04", "swk03",
+					"swk02" }, { "sgr01", "sgr02", "sgr03", "sgr04" },
+			{ "trg01", "trg04", "trg03", "trg02" }, { "wly02" } };
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.zon_solat);
+
+		View selectNegeriButton = findViewById(R.id.select_negeri);
+		selectNegeriButton.setOnClickListener(this);
+
+		View selectKawasanButton = findViewById(R.id.select_kawasan);
+		selectKawasanButton.setOnClickListener(this);
+
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.select_negeri:
+			openNegeriDialog();
+			break;
+		case R.id.select_kawasan:
+			openKawasanDialog();
+			break;
+		}
+	}
+
+	private void openNegeriDialog() {
+		new AlertDialog.Builder(this).setTitle(R.string.select_negeri_default)
+				.setItems(negeri, new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialoginterface, int i) {
+						selectnegeri(i);
+					}
+				}).show();
+	}
+
+	private void selectnegeri(int i) {
+		Log.d(TAG, "selected negeri " + i);
+		getPreferences(MODE_PRIVATE).edit().putLong("negeri", i).commit();
+		final Button button = (Button) findViewById(R.id.select_negeri);
+		button.setText(negeri[i]);
+	}
+
+	private void openKawasanDialog() {
+		new AlertDialog.Builder(this)
+				.setTitle(R.string.select_kawasan_default)
+				.setItems(
+						nama_kawasan[(int) getPreferences(MODE_PRIVATE)
+								.getLong("negeri", 0)],
+						new DialogInterface.OnClickListener() {
+							public void onClick(
+									DialogInterface dialoginterface, int i) {
+								selectkawasan(i);
+							}
+						}).show();
+	}
+
+	private void selectkawasan(int i) {
+		Log.d(TAG, "selected kawasan " + i);
+		getPreferences(MODE_PRIVATE).edit().putLong("kawasan", i).commit();
+		Intent intent = new Intent(this, waktusolat.class);
+		intent.putExtra("kod_kawasan", kod_kawasan[(int) getPreferences(MODE_PRIVATE).getLong("negeri", 0)][i]);
+		intent.putExtra("kawasan", nama_kawasan[(int) getPreferences(MODE_PRIVATE).getLong("negeri", 0)][i]);
+		intent.putExtra("negeri", negeri[(int) getPreferences(MODE_PRIVATE).getLong("negeri", 0)]);
+		startActivity(intent);
+	}
+
+}
---
+++ src/com/abdullahsolutions/solatmalaysia/waktusolat.java
@@ -0,0 +1,202 @@
+package com.abdullahsolutions.solatmalaysia;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.RejectedExecutionException;
+
+import com.abdullahsolutions.solatmalaysia.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+
+import android.text.format.DateFormat;
+import android.util.Log;
+
+public class waktusolat extends Activity implements OnClickListener {
+
+	private static final String TAG = "waktusolat";
+	private static final String WAKTU_PREF = "savedwaktu";
+
+	private TextView imsak_time, subuh_time, syuruk_time, zohor_time,
+			asar_time, maghrib_time, isya_time,kawasan,negeri,kemaskini;
+	public static final String waktu_imsak = "waktu_imsak";
+	public static final String waktu_subuh = "waktu_subuh";
+	public static final String waktu_syuruk = "waktu_syuruk";
+	public static final String waktu_zohor = "waktu_zohor";
+	public static final String waktu_asar = "waktu_asar";
+	public static final String waktu_maghrib = "waktu_maghrib";
+	public static final String waktu_isya = "waktu_isya";
+	public static final String waktu_kemaskini = "kemaskini";	
+
+	private Handler guiThread;
+	private ExecutorService waktuThread;
+	private Runnable updatewaktu;
+	private Future waktupending;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);		
+		
+		if (getIntent().hasExtra("kod_kawasan")) {
+			Bundle extras = getIntent().getExtras();
+			String kod_kawasan = extras.getString("kod_kawasan");
+			getPreferences(MODE_PRIVATE).edit().putString("kod_kawasan", kod_kawasan).commit();
+			
+			String kawasan = extras.getString("kawasan");
+			getPreferences(MODE_PRIVATE).edit().putString("kawasan", kawasan).commit();
+			
+			String negeri = extras.getString("negeri");
+			getPreferences(MODE_PRIVATE).edit().putString("negeri", negeri).commit();
+			Log.d(TAG, "kod_kawasan:" + kod_kawasan);
+		}
+		View selectZoneButton = findViewById(R.id.select_zone);
+		selectZoneButton.setOnClickListener(this);
+
+		initThreading();
+		findViews();
+		guiThread.post(updatewaktu);
+	}
+
+	private void findViews() {		
+		
+		imsak_time = (TextView) findViewById(R.id.imsak_time);
+		subuh_time = (TextView) findViewById(R.id.subuh_time);
+		syuruk_time = (TextView) findViewById(R.id.syuruk_time);
+		zohor_time = (TextView) findViewById(R.id.zohor_time);
+		asar_time = (TextView) findViewById(R.id.asar_time);
+		maghrib_time = (TextView) findViewById(R.id.maghrib_time);
+		isya_time = (TextView) findViewById(R.id.isya_time);
+		negeri = (TextView) findViewById(R.id.negeri);
+		kawasan = (TextView) findViewById(R.id.kawasan);
+		kemaskini = (TextView) findViewById(R.id.kemaskini);
+		
+		negeri.setText(getPreferences(MODE_PRIVATE).getString("negeri", "Selangor Dan Wilayah Persekutuan"));
+		kawasan.setText(getPreferences(MODE_PRIVATE).getString("kawasan", "Kuala Lumpur"));
+		kemaskini.setText(getPreferences(MODE_PRIVATE).getString("kemaskini", "Belum Pernah"));
+	}
+
+	private void initThreading() {
+		guiThread = new Handler();
+		waktuThread = Executors.newSingleThreadExecutor();
+		updatewaktu = new Runnable() {
+			public void run() {
+				String curdate = (String) android.text.format.DateFormat.format("dd/MM/yyyy", new java.util.Date());				
+				if(getIntent().hasExtra("kod_kawasan") || !curdate.equals(getPreferences(MODE_PRIVATE).getString("kemaskini", "Belum Pernah").substring(9))){
+					if (waktupending != null)
+						waktupending.cancel(true);
+					try {
+						waktusolatTask waktutask = new waktusolatTask(waktusolat.this);
+						waktupending = waktuThread.submit(waktutask);
+					} catch (RejectedExecutionException e) {
+						Log.d(TAG, "Rejectedexception", e);
+					}
+				}
+				else{
+					updatewaktu();
+				}
+			}
+		};
+	}
+
+	public void savewaktu(String nama_waktu, String waktu) {
+		getPreferences(MODE_PRIVATE).edit().putString(nama_waktu, waktu).commit();
+	}
+	
+	public void savekemaskini() {
+		String curdate = (String) android.text.format.DateFormat.format("hh:mm a dd/MM/yyyy", new java.util.Date());
+		getPreferences(MODE_PRIVATE).edit().putString("kemaskini", curdate).commit();
+	}
+
+	public void updatewaktu() {
+		guiThread.post(new Runnable() {
+			public void run() {
+				SimpleDateFormat cdf = new SimpleDateFormat("hh:mm");
+				try{
+					String curtime = (String) android.text.format.DateFormat.format("hh:mm", new java.util.Date());
+					Date curdate = cdf.parse(curtime);
+					
+					String strimsak = getPreferences(MODE_PRIVATE).getString(waktu_imsak, "--");
+					String strsubuh = getPreferences(MODE_PRIVATE).getString(waktu_subuh, "--");
+					String strsyuruk = getPreferences(MODE_PRIVATE).getString(waktu_syuruk, "--");
+					String strzohor = getPreferences(MODE_PRIVATE).getString(waktu_zohor, "--");
+					String strasar = getPreferences(MODE_PRIVATE).getString(waktu_asar, "--");
+					String strmaghrib = getPreferences(MODE_PRIVATE).getString(waktu_maghrib, "--");
+					String strisya = getPreferences(MODE_PRIVATE).getString(waktu_isya, "--");
+					
+					imsak_time.setText(strimsak);
+					if(curdate.after(cdf.parse(strimsak)) && curdate.before(cdf.parse(strsubuh))){
+						TextView title = (TextView) findViewById(R.id.imsak_title);
+						title.setBackgroundResource(R.color.hightlight);
+						imsak_time.setBackgroundResource(R.color.hightlight);
+					}
+					subuh_time.setText(strsubuh);
+					if(curdate.after(cdf.parse(strsubuh)) && curdate.before(cdf.parse(strsyuruk))){
+						TextView title = (TextView) findViewById(R.id.subuh_title);
+						title.setBackgroundResource(R.color.hightlight);
+						subuh_time.setBackgroundResource(R.color.hightlight);
+					}
+					syuruk_time.setText(strsyuruk);
+					if(curdate.after(cdf.parse(strsyuruk)) && curdate.before(cdf.parse(strzohor))){
+						TextView title = (TextView) findViewById(R.id.syuruk_title);
+						title.setBackgroundResource(R.color.hightlight);
+						syuruk_time.setBackgroundResource(R.color.hightlight);
+					}
+					zohor_time.setText(strzohor);
+					if(curdate.after(cdf.parse(strzohor)) && curdate.before(cdf.parse(strasar))){
+						TextView title = (TextView) findViewById(R.id.zohor_title);
+						title.setBackgroundResource(R.color.hightlight);
+						zohor_time.setBackgroundResource(R.color.hightlight);
+					}
+					asar_time.setText(strasar);
+					if(curdate.after(cdf.parse(strasar)) && curdate.before(cdf.parse(strmaghrib))){
+						TextView title = (TextView) findViewById(R.id.asar_title);
+						title.setBackgroundResource(R.color.hightlight);
+						asar_time.setBackgroundResource(R.color.hightlight);
+					}
+					maghrib_time.setText(strmaghrib);
+					if(curdate.after(cdf.parse(strmaghrib)) && curdate.before(cdf.parse(strisya))){
+						TextView title = (TextView) findViewById(R.id.maghrib_title);
+						title.setBackgroundResource(R.color.hightlight);
+						maghrib_time.setBackgroundResource(R.color.hightlight);
+					}
+					isya_time.setText(strisya);
+					if(curdate.after(cdf.parse(strisya))){
+						TextView title = (TextView) findViewById(R.id.isya_title);
+						title.setBackgroundResource(R.color.hightlight);
+						isya_time.setBackgroundResource(R.color.hightlight);
+					}
+				}
+				catch(Exception e){
+					
+				}
+				
+				kemaskini.setText(getPreferences(MODE_PRIVATE).getString("kemaskini", "Belum Pernah"));
+			}
+		});
+	}
+
+	public String getkodkawasan() {
+		return getPreferences(MODE_PRIVATE).getString("kod_kawasan", "sgr03");
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.select_zone:
+			Intent i = new Intent(this, ZonSolat.class);
+			startActivity(i);
+			break;
+		}
+	}
+}
